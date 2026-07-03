8d13a4e12cfb541e2a4d782c47a52fd54aa7db7a
parkguncherl <luckeey21@gmail.com>
2026-07-03 14:48:19 +0900

    fool

--- app/(shop)/guide/[slug]/PersonalColorGuide.tsx
+++ app/(shop)/guide/[slug]/PersonalColorGuide.tsx
@@ -5,7 +5,7 @@ export default function PersonalColorGuide() {
   return (
     <article className={styles.article}>
       <section className={styles.section}>
-        <img src="/images/guide/coolwarm.png" alt="쿨톤 웜톤 피부톤 확인 방법" className={styles.guideImg} />
+        <img src="/images/guide/skintone_guide.png" alt="쿨톤 웜톤 피부톤 확인 방법" className={styles.guideImg} />
       </section>
       <section className={styles.section}>
         <img src="/images/guide/seasonal.png" alt="4계절 퍼스널 컬러 색환" className={styles.guideImg} />
